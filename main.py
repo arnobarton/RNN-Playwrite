f7cf1d29912d0a93b12308528cb6d10e6576d03e
Arno Barton <70728194+arnobarton@users.noreply.github.com>
2021-10-29 18:16:17 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,146 @@
+%tensorflow_version 2.x  # this line is not required unless you are in a notebook
+from keras.preprocessing import sequence
+import keras
+import tensorflow as tf
+import os
+import numpy as np
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+from google.colab import files
+path_to_file = list(files.upload().keys())[0] #use this if you want to upload a peice of your own text
+
+# Read, then decode for py2 compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print ('Length of text: {} characters'.format(len(text)))
+
+vocab = sorted(set(text))
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+def text_to_int(text):
+  return np.array([char2idx[c] for c in text])
+
+text_as_int = text_to_int(text)
+
+def int_to_text(ints):
+  try:
+    ints = ints.numpy()
+  except:
+    pass
+  return ''.join(idx2char[ints])
+
+seq_length = 100  # length of sequence for a training example
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk): 
+    input_text = chunk[:-1] 
+    target_text = chunk[1:]  
+    return input_text, target_text  
+
+dataset = sequences.map(split_input_target)  
+
+for x, y in dataset.take(2):
+  print("\n\nEXAMPLE\n")
+  print("INPUT")
+  print(int_to_text(x))
+  print("\nOUTPUT")
+  print(int_to_text(y))
+  
+BATCH_SIZE = 64
+VOCAB_SIZE = len(vocab)  #umber of unique characters
+EMBEDDING_DIM = 256
+RNN_UNITS = 1024
+
+
+BUFFER_SIZE = 10000
+
+data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.LSTM(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(VOCAB_SIZE,EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+for input_example_batch, target_example_batch in data.take(1):
+  example_batch_predictions = model(input_example_batch)  # ask our model for a prediction on our first batch of training data (64 entries)
+# one prediction
+pred = example_batch_predictions[0]
+time_pred = pred[0]
+#  determine the predicted character sample the output distribution
+sampled_indices = tf.random.categorical(pred, num_samples=1)
+
+
+sampled_indices = np.reshape(sampled_indices, (1, -1))[0]
+predicted_chars = int_to_text(sampled_indices)
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+model.compile(optimizer='adam', loss=loss)
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+history = model.fit(data, epochs=50, callbacks=[checkpoint_callback]) #suggested to use 50 epochs.minimum of 25 if ur short on time but still want it good
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+checkpoint_num = 10
+model.load_weights(tf.train.load_checkpoint("./training_checkpoints/ckpt_" + str(checkpoint_num)))
+model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+
+
+  num_generate = 800
+
+  # vectorizing
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  
+  text_generated = []
+
+
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+    
+      predictions = tf.squeeze(predictions, 0)
+
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+
+inp = input("Type a starting string: ")
+print(generate_text(model, inp))
